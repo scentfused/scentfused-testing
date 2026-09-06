71c36740ad1ebd0bad8e07a525a1674c0733beb2
scentfused <scentfused@gmail.com>
2026-09-07 04:17:32 +0500

    Update settings.js

--- src/data/settings.js
+++ src/data/settings.js
@@ -2,13 +2,20 @@ export const FONT_OPTIONS = [
   'Audiowide',
   'Robot Monster',
   'Royale',
+  'Orange Avenue',
+  'Royale_Sb',
+  'Royale_BOLD',
+  'SS Royal',
+  'Senda Display',
+  'Shanoy',
+  'Soviet Style',
   'Orbitron',
   'Bebas Neue',
   'Cinzel',
   'Metal Mania',
   'Monoton'  
-/*  'Orange Avenue',
-  'Royale',
+/*
+  'Orange Avenue',
   'Royale_Sb',
   'Royale_BOLD',
   'SS Royal',
@@ -18,7 +25,7 @@ export const FONT_OPTIONS = [
 ]
 
 export const defaultSettings = {
-  brandFont: 'Royale',
+  brandFont: 'Robot Monster',
   accentColor: '#d4af37',
   showNewBadge: true,
   carouselAutoplay: true
