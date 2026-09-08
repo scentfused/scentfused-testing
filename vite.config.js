641d208fe586f6c705ee2043d96e02bd77973fac
scentfused <scentfused@gmail.com>
2026-09-08 17:50:21 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // or your respective framework plugin
-
-export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    // This forces esbuild to treat .js files as .jsx
-    loader: 'jsx',
-    include: /src\/.*\.js$/, 
-  },
-});
-
-/* 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()]
 })
-*/
