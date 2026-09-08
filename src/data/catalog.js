ae1eddf142c70256e4f7635adb1c66ad249b4da0
scentfused <scentfused@gmail.com>
2026-09-08 17:31:15 +0500

    Update catalog.js

--- src/data/catalog.js
+++ src/data/catalog.js
@@ -51,8 +51,3 @@ export const latestNames = [
 export function emptyDraft() {
   return { name: '', category: 'perfumes', note: '', price: '', image: '', variants: [] }
 }
-
-/*i have added the new code above
-export function emptyDraft() {
-  return { name: '', category: 'perfumes', note: '', price: '', image: '' } */
-
