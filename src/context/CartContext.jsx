479a4ff2dc74aa7d21fd5fe95d23feecbea4d757
scentfused <scentfused@gmail.com>
2026-09-09 18:58:10 +0500

    Update CartContext.jsx

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,1 +1,38 @@
+import { createContext, useContext, useState } from "react";
 
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((currentCart) => [...currentCart, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((currentCart) =>
+      currentCart.filter((item) => item.id !== productId)
+    );
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  return useContext(CartContext);
+};
